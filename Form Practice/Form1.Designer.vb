c70df2d699e36469e59ff70fb0cf52c83689a5b8
SYAFE <ajk654@naver.com>
2020-09-17 11:59:21 +0900

    0917

---
+++ Form Practice/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtOK = New System.Windows.Forms.Button()
+        Me.BtCC = New System.Windows.Forms.Button()
+        Me.Textbox = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtOK
+        '
+        Me.BtOK.Location = New System.Drawing.Point(232, 348)
+        Me.BtOK.Name = "BtOK"
+        Me.BtOK.Size = New System.Drawing.Size(82, 35)
+        Me.BtOK.TabIndex = 0
+        Me.BtOK.Text = "OK"
+        Me.BtOK.UseVisualStyleBackColor = True
+        '
+        'BtCC
+        '
+        Me.BtCC.Location = New System.Drawing.Point(508, 348)
+        Me.BtCC.Name = "BtCC"
+        Me.BtCC.Size = New System.Drawing.Size(80, 35)
+        Me.BtCC.TabIndex = 1
+        Me.BtCC.Text = "終了"
+        Me.BtCC.UseVisualStyleBackColor = True
+        '
+        'Textbox
+        '
+        Me.Textbox.Location = New System.Drawing.Point(51, 33)
+        Me.Textbox.Name = "Textbox"
+        Me.Textbox.Size = New System.Drawing.Size(701, 266)
+        Me.Textbox.TabIndex = 2
+        Me.Textbox.Text = "確認してボタンを押してください"
+        Me.Textbox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Textbox)
+        Me.Controls.Add(Me.BtCC)
+        Me.Controls.Add(Me.BtOK)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BtOK As Button
+    Friend WithEvents BtCC As Button
+    Friend WithEvents Textbox As Label
+End Class
---
+++ Form Practice/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub BtOK_Click(sender As Object, e As EventArgs) Handles BtOK.Click
+        Textbox.Text = "OK"
+    End Sub
+
+    Private Sub BtCC_Click(sender As Object, e As EventArgs) Handles BtCC.Click
+        End
+    End Sub
+End Class
